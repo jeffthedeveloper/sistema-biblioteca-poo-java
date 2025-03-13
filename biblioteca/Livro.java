0110a0376311c5eb9475be7306939200542cbd1c
jeffthedeveloper <jeffersonfir@gmail.com>
2025-03-13 20:13:11 -0300

    adicionando o README.md file com mais detalhes e explicações do código

--- biblioteca/Livro.java
+++ biblioteca/Livro.java
@@ -4,14 +4,14 @@ public class Livro {
     private String titulo;
     private String autor;
     private String isbn;
-    private boolean disponível;
+    private boolean disponivel;
 
     public Livro(String titulo, String autor, String isbn) {
         this.titulo = titulo;
         this.autor = autor;
         this.isbn = isbn;
-        this.disponível = true; 
-        // O livro é criado disponível
+        this.disponivel = true; 
+        // O iivro é criadi disponível
     }
 
 // Métodos getters e setters para acessar e modificar os atributos
@@ -27,7 +27,7 @@ public class Livro {
         return autor;
     }
 
-    public void  setautor(String autor) {
+    public void  setAutor(String autor) {
         this.autor = autor;
     }
 
@@ -41,18 +41,18 @@ public class Livro {
     }
 
     public boolean isDisponível() {
-        return disponível;
+        return disponivel;
     }
 
-    public void setDisponivel(boolean disponível) {
-        this.disponível = disponível;
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
     }
 
     public void exibirDetalhes() {
         System.out.println("Título: " + titulo);
         System.out.println("Autor: " + autor);
         System.out.println("ISBN: " + isbn);
-        System.out.println("Disponível: " + (disponível ? "Sim" : "Não"));
+        System.out.println("Disponível: " + (disponivel ? "Sim" : "Não"));
         System.out.println();
 
     }
